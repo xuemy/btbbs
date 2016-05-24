624f0d1ec2373e6948559e5bb5982a0cf4937976
xuemy <xmy413465057@163.com>
2016-05-24 19:21:59 +0800

    添加了sitemap

--- bbs/urls.py
+++ bbs/urls.py
@@ -2,14 +2,42 @@
 from __future__ import unicode_literals
 
 from django.conf.urls import url
+from django.contrib.sitemaps import GenericSitemap
+from django.views.decorators.cache import cache_page
 
-from bbs import views
+from bbs import sitemap
+from bbs.models import Movie
+from bbs.views import category
+from bbs.views import detail
+from bbs.views import download
+from bbs.views import index
+from bbs.views import tag
+
+class LimitSitemap(GenericSitemap):
+    limit = 500
+
+
+sitemaps = {
+    'movie': LimitSitemap({'queryset': Movie.objects.all(), 'date_field': 'ctime'}, priority=0.6),
+}
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^detail/(?P<tid>\d+).html$', views.detail
-        , name='detail'),
-    url(r'^category/(?P<slug>[a-zA-Z]+).html$', views.category, name='category'),
-    url(r'^download/(?P<tid>\d+)$', views.download, name='download'),
-    url(r'^tag/(?P<tag>.+)$', views.tag, name='tag')
+    url(r'^$', index.index , name='index'),
+
+    url(r'^detail/(?P<tid>\d+).html$', detail.V.as_view(), name='detail'),
+
+    url(r'^download/(?P<tid>\d+)$', download.view, name='download'),
+
+    url(r'^tag/(?P<tag_name>.+)$', tag.Tag.as_view(), name='tag'),
+
+    url(r'^(?P<category>[tvmovie])', category.Category.as_view(), name='category'),
+
+
+
+    url(r'sitemap\.xml$',
+        cache_page(86400)(sitemap.index),
+        {'sitemaps': sitemaps, 'sitemap_url_name': 'sitemaps'}),
+    url(r'sitemap-(?P<section>.+)-(?P<page>\d+)\.xml',
+        cache_page(86400)(sitemap.sitemap),
+        {'sitemaps': sitemaps}, name='sitemaps'),
 ]
